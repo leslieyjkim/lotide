5a7164e41dca5ac8b8a026a636e142a52325e611
yoonjookim <yjblossom@gmail.com>
2023-11-14 10:18:18 -0500

    updated using the template literals

--- assertEqual.js
+++ assertEqual.js
@@ -13,13 +13,27 @@
 //Above coding what I made first was failed
 //Below coding is revised one.
 
+// const assertEqual = function (actual, expected) {
+//   console.assert(
+//     actual === expected,
+//     "❌ Assertion Failed: " + actual + " !== " + expected
+//   );
+//   if (actual === expected) {
+//     console.log("✅ Assertion Passed: " + actual + " === " + expected);
+//   }
+// };
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+//Below would be revised one using a template literals
 const assertEqual = function (actual, expected) {
   console.assert(
     actual === expected,
-    "❌ Assertion Failed: " + actual + " !== " + expected
+    `❌ Assertion Failed ${actual} !== ${expected}`
   );
   if (actual === expected) {
-    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   }
 };
 
