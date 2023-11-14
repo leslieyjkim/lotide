c69ec97a14a309964804330a871eb617a3367f80
yoonjookim <yjblossom@gmail.com>
2023-11-14 15:46:32 -0500

    revised based on AI code review: array.slice();

--- tail.js
+++ tail.js
@@ -26,13 +26,22 @@ assertEqual(head([]), undefined); //test with an empty array
 //everything except for the first item (head) of the provided array.
 
 //There is a workaround to this problem where we could still use our simple assertEqual to test out our tail function. How? Spend some time to think of and/or discuss a possible solution with your peers prior to exposing the answer below.
+
+// const tail = function(array) {
+//   if (array.length <= 1) {
+//     return [];
+//   }
+//   return array.slice(1);
+// };
+
+//code review: (above code revised) In your tail function, you're returning an empty array when the length of the input array is less than or equal to 1. While this works for an empty array, it doesn't quite meet the requirement for an array with a single element. The requirement states that "using an array with one or zero elements as an argument returns an empty array". Your current implementation will return an empty array for an array with one element, but it should actually return the original array. You might want to adjust your condition to handle this case correctly.
+//revised below;
 const tail = function (array) {
   if (array.length <= 1) {
-    return [];
+    return array.slice(); // Return a shallow copy of the array
   }
   return array.slice(1);
 };
-
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 //Here, the tail function is called with an array ["Hello", "Lighthouse", "Labs"], and the result is stored in the variable result. The purpose of this line is to obtain the output of the tail function for further testing.
 assertEqual(result.length, 2); // ensure we get back two elements
