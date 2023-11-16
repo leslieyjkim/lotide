d3a62ae0606d5cca3fad5933136f978bf0f11794
yoonjookim <yjblossom@gmail.com>
2023-11-16 15:33:38 -0500

    revised and modified under the AI review

--- countLetters.js
+++ countLetters.js
@@ -8,14 +8,18 @@ const assertEqual = function (actual, expected) {
 
 // eslint-disable-next-line func-style
 function countLetters(sentence) {
-  const lowercaseCount = {};
-  const uppercaseCount = {};
+  const letterCount = {
+    lowercase: {},
+    uppercase: {},
+  };
 
   for (const letter of sentence) {
     if (/^[a-zA-Z]$/.test(letter)) {
       ////^[a-zA-Z]$ is used to test whether a given character letter is an uppercase or lowercase letter.
       const isUpperCase = letter === letter.toUpperCase(); //Check if the letter is uppercase or lowercase
-      const countObject = isUpperCase ? uppercaseCount : lowercaseCount; //Update the count for the letter based on case
+      const countObject = isUpperCase
+        ? letterCount.uppercase
+        : letterCount.lowercase; //Update the count for the letter based on case
       const key = isUpperCase ? letter : letter.toLowerCase(); //key will be assigned the value of letter if isUppercase is true, and the lowercase version of letter otherwise.
 
       if (key in countObject) {
@@ -29,18 +33,24 @@ function countLetters(sentence) {
       // countObject[key] = (countObject[key] || 0) + 1;
     }
   }
-  return { lowercase: lowercaseCount, uppercase: uppercaseCount };
+  // eslint-disable-next-line no-undef
+  return letterCount;
 }
 
-const sentence = "lighthouse Labs";
-const result = countLetters(sentence);
+// const sentence = "lighthouse Labs";
+// const result = countLetters(sentence);
 
-console.log("Lowercase counts:");
-for (const letter in result.lowercase) {
-  console.log(`${letter}: ${result.lowercase[letter]} times`);
-}
+// console.log("Lowercase counts:");
+// for (const letter in result.lowercase) {
+//   console.log(`${letter}: ${result.lowercase[letter]} times`);
+// }
 
-console.log("\nUppercase counts:");
-for (const letter in result.uppercase) {
-  console.log(`${letter}: ${result.uppercase[letter]} times`);
-}
+// console.log("\nUppercase counts:");
+// for (const letter in result.uppercase) {
+//   console.log(`${letter}: ${result.uppercase[letter]} times`);
+// }
+//
+
+const result = countLetters("lighthouse Labs");
+assertEqual(result.lowercase["l"], 2, "Count of 'l' (lowercase) is 2");
+assertEqual(result.uppercase["L"], 1, "Count of 'L' (uppercase) is 1");
