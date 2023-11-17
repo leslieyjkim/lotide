2383fa885d392d77741c65878cf7a5007a281150
yoonjookim <yjblossom@gmail.com>
2023-11-16 20:24:28 -0500

    revised using 'Object.keys(object)

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (object, value) {
+  const keys = Object.keys(object); //get an array of object's keys to iterate
+  for (const key of keys) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
