47c6d478bfd3161b8f11d99ed95fb17b73be64dd
yoonjookim <yjblossom@gmail.com>
2023-11-21 23:47:48 -0500

    modified

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const eqObjects = require("./eqObjects");
 const findKey = require("./findKey");
 const findKeyByValue = require("./findKeyByValue");
 const letterPositions = require("./letterPositions");
-const map = require("./map");
 const takeUntil = require("./takeUntil");
 const without = require("./without");
 
@@ -29,7 +28,6 @@ module.exports = {
   findKey: findKey,
   findKeyByValue: findKeyByValue,
   letterPositions: letterPositions,
-  map: map,
   takeUntil: takeUntil,
   without: without,
 };
