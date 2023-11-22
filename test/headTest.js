97fa29bfcf9f0af1577e3cee4cc59c4f04163b95
yoonjookim <yjblossom@gmail.com>
2023-11-21 22:48:54 -0500

    M&C converted headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,33 @@
-const head = require("../head");
-const assertEqual = require("../assertEqual");
+// const head = require("../head");
+// const assertEqual = require("../assertEqual");
 
 //test cases
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([42]), 42); // Test with an array containing one element
-assertEqual(head([]), undefined); //test with an empty array
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([42]), 42); // Test with an array containing one element
+// assertEqual(head([]), undefined); //test with an empty array
+
+const assert = require("chai").assert;
+const head = require("../head");
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(["5"]), "5");
+  });
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  it("returns 42 for [42]", () => {
+    assert.strictEqual(head([42]), 42);
+  });
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
