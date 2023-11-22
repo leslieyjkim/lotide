b42478a086e6059530ce93839fb31c0a64da19eb
yoonjookim <yjblossom@gmail.com>
2023-11-21 23:06:04 -0500

    M&C converted middleTest and tailTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,19 +1,52 @@
+// const middle = require("../middle");
+// const assertArraysEqual = require("../assertArraysEqual");
+
+// //test code
+// assertArraysEqual(middle([]), []); // should pass
+
+// // Test case 2: One element
+// assertArraysEqual(middle([1]), []); // should pass
+
+// // Test case 3: Two elements
+// assertArraysEqual(middle([1, 2]), []); // should pass
+
+// // Test case 4: Odd number of elements
+// assertArraysEqual(middle([1, 2, 3]), [2]); // should pass
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // should pass
+
+// // Test case 5: Even number of elements
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // should pass
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // should pass
+
+const assert = require("chai").assert;
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
 
-//test code
-assertArraysEqual(middle([]), []); // should pass
+describe("#middle", () => {
+  it("returns an empty array for an empty array", () => {
+    assert.deepEqual(middle([]), []);
+  });
 
-// Test case 2: One element
-assertArraysEqual(middle([1]), []); // should pass
+  it("returns an empty array for an array with one element", () => {
+    assert.deepEqual(middle([1]), []);
+  });
 
-// Test case 3: Two elements
-assertArraysEqual(middle([1, 2]), []); // should pass
+  it("returns an empty array for an array with two elements", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
 
-// Test case 4: Odd number of elements
-assertArraysEqual(middle([1, 2, 3]), [2]); // should pass
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // should pass
+  it("returns [2] for an array with three elements [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
 
-// Test case 5: Even number of elements
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // should pass
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // should pass
+  it("returns [3] for an array with five elements [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("returns [2, 3] for an array with four elements [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns [3, 4] for an array with six elements [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,17 +1,29 @@
+// const tail = require("../tail");
+// const assertEqual = require("../assertEqual");
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2);
+// assertEqual(result[0], "Lighthouse");
+// assertEqual(result[1], "Labs");
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+const assert = require("chai").assert;
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-//Here, the tail function is called with an array ["Hello", "Lighthouse", "Labs"], and the result is stored in the variable result. The purpose of this line is to obtain the output of the tail function for further testing.
-assertEqual(result.length, 2); // ensure we get back two elements
-//This line uses assertEqual to check whether the length of the result array is equal to 2. The tail function is expected to return an array with all elements except the first one, so in this specific case, the original array has 3 elements, and the tail should have 2 elements.
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-// This line checks whether the first element of the result array is equal to the string "Lighthouse". Since the tail function is expected to exclude the first element, this assertion ensures that the first element of the tail is indeed "Lighthouse".
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-// Similarly, this line checks whether the second element of the result array is equal to the string "Labs". Again, because the tail function is designed to exclude the first element, this assertion verifies that the second element of the tail is "Labs".
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result.length, 2);
+    assert.strictEqual(result[0], "Lighthouse");
+    assert.strictEqual(result[1], "Labs");
+  });
 
-// In summary, these assertions collectively verify the expected behavior of the tail function: it returns an array with the correct length and contains specific elements that were originally the second and third elements of the input array.
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("does not modify the original array ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
+});
